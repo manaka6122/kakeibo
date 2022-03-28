28918b6bce68603e55029782f0b44bfffc9fbc88
EC2 Default User <ec2-user@ip-172-31-24-219.ec2.internal>
2022-03-28 12:08:43 +0000

    [Add]会員登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
+  
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  get "/signin", to: "session#new"
+  post "/signin", to: "session#create"
 end
